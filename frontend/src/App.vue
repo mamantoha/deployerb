b90cff4dec6cb67cb08dcae43791cd08e2dd6926
Anton Maminov <anton.linux@gmail.com>
2025-02-08 17:59:00 +0200

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
         <router-link class="navbar-brand" to="/">Deployerb</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
